0f3309a83064b1fd4b027c3b94eb4eb62da6d6aa
Sunday Robert <pelurobert10@gmail.com>
2024-03-18 16:52:41 +0100

    first commit - the signup page

---
+++ src/components/Auth/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Tracker</h1>
+      <h3>Welcome Back</h3>
+      <p> Please sign into your account</p>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/components/Auth/Signup/SignUp.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import './signup.css'
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z
+  .object({
+    email: z.string().email(),
+    fullName: z.string().min(1, { message: "field is required" }),
+    userName: z.string().min(1, { message: "field is required" }),
+    password: z
+      .string()
+      .min(8, { message: "password must be at keast 8 characers" })
+      .max(20, { message: "Password is too long" }),
+    confirmPassword: z.string().min(1, { message: "password is required" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password do not match",
+    path: ["confirmPassword"],
+  });
+
+const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    setError,
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = async (data) => {
+    await new Promise((resolve, reject) => {});
+    console.log(data);
+  };
+  return (
+    <div className="formContainer">
+      <div className="box">
+      <h1>Tracker</h1>
+      <h3>Welcome!</h3>
+      <p>kindly create your account</p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+        <input
+          name="fullName"
+          id="fullName"
+          {...register("fullName")}
+          type="text"
+          placeholder="Full name"
+        />
+        {errors.fullName && <div className="errorMessage"> {errors.fullName?.message} </div>}
+        </div>
+        <div>
+        <input
+          id="userName"
+          {...register("userName")}
+          type="text"
+          placeholder="Username"
+        />      
+        {errors.userName && <div className="errorMessage"> {errors.fullName?.message} </div>}
+        </div>
+        <div>
+        <input
+          id="email"
+          {...register("email")}
+          type="email"
+          placeholder="Email Address"
+        />
+        {errors.email && <div className="errorMessage"> {errors.email?.message} </div>}
+        </div>
+        <div>
+        <input
+          id="password"
+          {...register("password")}
+          type="password"
+          placeholder="Password"
+        />
+        {errors.password && <div className="errorMessage"> {errors.password?.message} </div>}
+        </div>
+        <div>
+        <input
+          id="c_password"
+          {...register("confirmPassword")}
+          type="password"
+          placeholder="Confirm Password"
+        />
+        {errors.confirmPassword && <div className="errorMessage"> {errors.confirmPassword?.message} </div>}
+        </div>
+        
+        <button disabled={isSubmitting} type="submit">
+          {" "}
+          {isSubmitting ? "Loading..." : "Sign Up"}{" "}
+        </button>
+      </form>
+      <div className="enter">
+      <p>Already have an account? <span>Log in.</span></p>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+// do throw error
+export default SignUp;
